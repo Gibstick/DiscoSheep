d7b075d507b09e1b86b4c232fed629082f3343c8
Charlie Wang <gibsticker@gmail.com>
2013-06-30 14:17:41 -0400

    All DiscoSheep methods are now package-protected

--- src/gibstick/bukkit/discosheep/DiscoSheep.java
+++ src/gibstick/bukkit/discosheep/DiscoSheep.java
@@ -24,7 +24,7 @@ public final class DiscoSheep extends JavaPlugin {
         // Watashi Wa Kawaii, Ne?
     }
 
-    public void spawnSheep(World world, Location loc) {
+    void spawnSheep(World world, Location loc) {
         Sheep newSheep = (Sheep) world.spawnEntity(loc, EntityType.SHEEP);
         newSheep.setMaxHealth(10000);
         newSheep.setHealth(10000);
@@ -32,7 +32,7 @@ public final class DiscoSheep extends JavaPlugin {
     }
 
     // Spawn some number of sheep next to given player
-    public void spawnSheep(Player player, int num) {
+    void spawnSheep(Player player, int num) {
         Location loc;
         World world = player.getWorld();
 
@@ -51,7 +51,7 @@ public final class DiscoSheep extends JavaPlugin {
     }
 
     // Mark all sheep in the sheep array for removal
-    public void removeAllSheep() {
+    void removeAllSheep() {
         for (int i = 0; i < sheepArray.size(); i++) {
             sheepArray.get(i).remove();
         }
@@ -59,17 +59,17 @@ public final class DiscoSheep extends JavaPlugin {
     }
 
     // Cycle colours of all sheep in the array
-    public void cycleSheepColours() {
+    void cycleSheepColours() {
         for (int i = 0; i < sheepArray.size(); i++) {
             //sheepArray.get(i) something something
         }
     }
 
-    public void playSounds() {
+    void playSounds() {
         // TODO: generate list of players to send sounds to
     }
 
-    public void playSounds(Player player) {
+    void playSounds(Player player) {
         //TODO: Add sound playing here
     }
 
