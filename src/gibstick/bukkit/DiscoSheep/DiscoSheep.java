ac22a680a1a1467bc994205c455404d860793b13
Charlie Wang <gibsticker@gmail.com>
2013-06-30 09:50:50 -0400

    Default class

---
+++ src/gibstick/bukkit/DiscoSheep/DiscoSheep.java
@@ -0,0 +1,20 @@
+package gibstick.bukkit.DiscoSheep;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Charlie
+ */
+public final class DiscoSheep extends JavaPlugin {
+    
+    @Override
+    public void onEnable(){
+        
+    }
+    
+    @Override
+    public void onDisable(){
+        
+    }
+}
