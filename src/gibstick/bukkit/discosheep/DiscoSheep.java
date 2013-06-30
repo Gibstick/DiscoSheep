23d88c7808ec19dbd9c399442c69b02ab4e6bd4d
Charlie Wang <gibsticker@gmail.com>
2013-06-30 12:11:54 -0400

    Implemented spawnSheep method that stores the spawned sheep in the sheep array

--- src/gibstick/bukkit/discosheep/DiscoSheep.java
+++ src/gibstick/bukkit/discosheep/DiscoSheep.java
@@ -3,10 +3,13 @@ package gibstick.bukkit.DiscoSheep;
 import java.util.ArrayList;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Sheep;
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+import org.bukkit.World;
 
 public final class DiscoSheep extends JavaPlugin {
     
-    private ArrayList<Sheep> SheepArray = new ArrayList<>();
+    private ArrayList<Sheep> sheepArray = new ArrayList<>();
     
     @Override
     public void onEnable(){
@@ -17,6 +20,10 @@ public final class DiscoSheep extends JavaPlugin {
     public void onDisable(){
         
     } 
+    
+    public void spawnSheep(World world, Location loc) {
+        sheepArray.add((Sheep)world.spawnEntity(loc, EntityType.SHEEP));
+    }
     
     
 }
