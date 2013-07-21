3ed151ed250f2fc7ae4ba0a37a1cf35dd146a5b9
Gibstick <gibsticker@gmail.com>
2013-07-21 11:41:21 -0400

    removed unused import

--- src/ca/gibstick/discosheep/DiscoSheepCommandExecutor.java
+++ src/ca/gibstick/discosheep/DiscoSheepCommandExecutor.java
@@ -1,7 +1,6 @@
 package ca.gibstick.discosheep;
 
 import java.util.Arrays;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
